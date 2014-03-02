85eb736e9c9046077cae2104b4b428597e9c49c3
Chris Lentini <lentinic@gmail.com>
2014-03-02 15:23:33 -0800

    Adding shared_lock class for scoped shared_mutex control

---
+++ include/taco/shared_lock.h
@@ -0,0 +1,103 @@
+/*
+Copyright (c) 2014 Chris Lentini
+http://divergentcoder.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of 
+this software and associated documentation files (the "Software"), to deal in 
+the Software without restriction, including without limitation the rights to use, 
+copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the 
+Software, and to permit persons to whom the Software is furnished to do so, 
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all 
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANT*IES OF MERCHANTABILITY, FITNESS 
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR 
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER 
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN 
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+#pragma once
+
+#include <basis/assert.h>
+
+namespace taco
+{
+	template<class MTYPE>
+	class shared_lock
+	{
+	public:
+		shared_lock(MTYPE & m)
+			:	m_mutex(&m),
+				m_locked(true)
+		{
+			m_mutex->lock_shared();
+		}
+
+		shared_lock(shared_lock && other)
+			:	m_mutex(other.m_mutex),
+				m_locked(other.m_locked)
+		{
+			other.m_mutex = nullptr;
+			other.m_locked = false;
+		}
+
+		shared_lock & operator = (shared_lock && other)
+		{
+			if (m_mutex && m_locked)
+			{
+				m_mutex->unlock_shared();
+			}
+
+			m_mutex = other.m_mutex;
+			m_locked = other.m_locked;
+			other.m_mutex = nullptr;
+			other.m_locked = false;
+
+			return *this;
+		}
+
+		~shared_lock()
+		{
+			if (m_locked)
+			{
+				BASIS_ASSERT(m_mutex != nullptr);
+				m_mutex->unlock_shared();
+			}
+		}
+
+		void try_lock()
+		{
+			BASIS_ASSERT(m_mutex != nullptr);
+			BASIS_ASSERT(!m_locked);
+			m_locked = m_mutex->try_lock_shared();
+			return m_locked;
+		}
+
+		void lock()
+		{
+			BASIS_ASSERT(m_mutex != nullptr);
+			BASIS_ASSERT(!m_locked);
+			m_mutex->lock_shared();
+			m_locked = true;
+		}
+
+		void unlock()
+		{
+			BASIS_ASSERT(m_mutex != nullptr);
+			BASIS_ASSERT(m_locked);
+			m_mutex->unlock();
+			m_locked = false;
+		}
+
+	private:
+		shared_lock(const shared_lock &);
+		shared_lock & operator = (const shared_lock &);
+
+		MTYPE * m_mutex;
+		bool	m_locked;
+	};
+}
