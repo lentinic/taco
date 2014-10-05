901cb19234b4b6723f44dfcc74ea152be5da6c57
Chris Lentini <lentinic@gmail.com>
2014-10-04 21:03:27 -0700

    Updated blocking test

--- tests/blocking.cpp
+++ tests/blocking.cpp
@@ -1,84 +1,152 @@
 #include <basis/unit_test.h>
+#include <basis/thread_util.h>
 #include <taco/taco.h>
 #include <algorithm>
 #include <iostream>
 
-void test_blocking();
+void test_independent();
+void test_dependent();
 
 BASIS_TEST_LIST_BEGIN()
-	BASIS_DECLARE_TEST(test_blocking)
+	//BASIS_DECLARE_TEST(test_independent)
+	BASIS_DECLARE_TEST(test_dependent)
 BASIS_TEST_LIST_END()
 
-#define MIN(a, b) ((a) < (b) ? (a) : (b))
+std::atomic<uint32_t> timewaiting[2];
 
-#define FILE_COUNT 64
-#define MAX_FILE_DIM 2048
-#define MAX_TASK_COUNT 512
-
-void test_function(bool blocking, unsigned ntasks, unsigned filelen)
+void dependent(bool blocking, unsigned ntasks, unsigned sleeptime, unsigned cputime)
 {
 	taco::future<uint32_t> * tasks = new taco::future<uint32_t>[ntasks];
 	for (unsigned i=0; i<ntasks; i++)
 	{
-			tasks[i] = taco::Start([=]() -> uint32_t {
-			uint32_t * data = nullptr;
+		tasks[i] = taco::Start([=]() -> uint32_t {
 			if (blocking) { taco::BeginBlocking(); }
-			{
-				char tmp[255];
-				sprintf(tmp, "tmp\\blocking_data%d.dat", i % FILE_COUNT);
-				FILE * file = fopen(tmp, "rb");
-				data = new uint32_t[filelen];
-				fread(data, 4, filelen, file);
-				fclose(file);
-			}
+
+			auto t0 = basis::GetTimestamp();
+			std::this_thread::sleep_for(std::chrono::microseconds(sleeptime));
+			timewaiting[blocking ? 0 : 1] += basis::GetTimestamp() - t0;
 			if (blocking) { taco::EndBlocking(); }
 
-			taco::future<uint32_t> mins[4];
-			for (size_t j = 0; j < 4; j++)
+			unsigned steps = 0;
+			for (unsigned j=0; j<cputime; j++)
 			{
-				mins[j] = taco::Start([=]() -> uint32_t {
-					uint32_t result = 0xffffffff;
-					size_t start = j * (filelen / 4);
-					size_t end = (j + 1) * (filelen / 4);
-					for (size_t k=start; k<end; k++)
-					{
-						result = MIN(data[k], result);
-					}
-					return result;
-				});
-			}	
-
-			uint32_t r = MIN(MIN(mins[0], mins[1]), MIN(mins[2], mins[3]));
-			delete [] data;
-			return r;
+				unsigned n = 837799;
+				while (n != 1)
+				{
+					n = (n & 1) ? ((3 * n) + 1) : (n / 2);
+					steps++;
+				}
+			}
+			return steps;
 		});
 	}
 	
-	for (int i=0; i<ntasks; i++)
+	for (unsigned i=1; i<ntasks; i++)
 	{
-		BASIS_TEST_VERIFY_MSG(tasks[i] == 0, "Should have equaled 0!");
+		BASIS_TEST_VERIFY_MSG(tasks[i] == tasks[i - 1], "Mismatch between %u and %u (%u vs %u)", 
+			i, i - 1, (uint32_t)tasks[i], (uint32_t)tasks[i - 1]);
 	}
 
 	delete [] tasks;
 }
 
-void test_blocking()
+void independent(bool blocking, unsigned ntasks, unsigned sleeptime, unsigned cputime)
+{
+	taco::future<void> * blockers = new taco::future<void>[ntasks];
+	for (unsigned i=0; i<ntasks; i++)
+	{
+		blockers[i] = taco::Start([=]() -> void {
+			if (blocking) { taco::BeginBlocking(); }
+
+			auto t0 = basis::GetTimestamp();
+			std::this_thread::sleep_for(std::chrono::microseconds(sleeptime));
+			timewaiting[blocking ? 0 : 1] += basis::GetTimestamp() - t0;
+			if (blocking) { taco::EndBlocking(); }
+		});
+	}
+ 
+	taco::future<unsigned> * calcers = new taco::future<unsigned>[ntasks];
+	for (unsigned i=0; i<ntasks; i++)
+	{
+		calcers[i] = taco::Start([=]() -> unsigned {
+			unsigned steps = 0;
+			for (unsigned j=0; j<cputime; j++)
+			{
+				unsigned n = 837799;
+				while (n != 1)
+				{
+					n = (n & 1) ? ((3 * n) + 1) : (n / 2);
+					steps++;
+				}
+			}
+			return steps;
+		});
+	}
+
+	blockers[0].await();
+	for (unsigned i=1; i<ntasks; i++)
+	{
+		blockers[i].await();
+		if (calcers[i].await() != calcers[i - 1].await())
+		{ 
+			printf("Hash mismatch between %u and %u!", i, i - 1);
+		}
+	}
+
+	delete [] blockers;
+	delete [] calcers;
+}
+
+void test_dependent()
 {
 	taco::Initialize([&]() -> void {
-		printf("Task Count\tFile Dim\tEnabled\tDisabled\tRelative\n");
-		for (unsigned tcount=128; tcount<=MAX_TASK_COUNT; tcount+=64)
+		timewaiting[0] = timewaiting[1] = 0;
+		printf("Task Count\tcpu\tsleep\tenabled\tdisabled\trelative\n");
+		for (unsigned tasks=64; tasks<=512; tasks*=2)
 		{
-			for (unsigned dim=128; dim<=MAX_FILE_DIM; dim+=64)
-			{
-				auto start = basis::GetTimestamp();
-				test_function(true, tcount, dim*dim);
-				auto enabled = basis::GetTimeDeltaMS(start, basis::GetTimestamp());
+			for (unsigned cpu=64; cpu<=2048; cpu*=4)
+			{	
+				for (unsigned sleep=16384; sleep<=(1<<18); sleep*=2)
+				{
+					auto start = basis::GetTimestamp();
+					dependent(true, tasks, sleep, cpu);
+					auto enabled = basis::GetTimeDeltaMS(start, basis::GetTimestamp());
 
-				start = basis::GetTimestamp();
-				test_function(false, tcount, dim*dim);
-				auto disabled = basis::GetTimeDeltaMS(start, basis::GetTimestamp());
+					start = basis::GetTimestamp();
+					dependent(false, tasks, sleep, cpu);
+					auto disabled = basis::GetTimeDeltaMS(start, basis::GetTimestamp());
 
-				printf("%u\t%u\t%u\t%u\t%.2f\n", tcount, dim, enabled, disabled, enabled / (float) disabled);
+					printf("%u\t%u\t%u\t%u\t%u\t%.2f\n", tasks, cpu, sleep, enabled, disabled, disabled / (float) enabled);
+				}
+			}
+		}
+	});
+
+	taco::Shutdown();
+}
+
+void test_independent()
+{
+	taco::Initialize([&]() -> void {
+		timewaiting[0] = timewaiting[1] = 0;
+		printf("Task Count\tcpu\tsleep\tenabled\tdisabled\trelative\n");
+		for (unsigned tasks=64; tasks<=512; tasks*=2)
+		{
+			for (unsigned cpu=64; cpu<=512; cpu*=2)
+			{	
+				for (unsigned sleep=1024; sleep<32768; sleep*=2)
+				{
+					auto start = basis::GetTimestamp();
+					independent(true, tasks, sleep, cpu);
+					auto enabled = basis::GetTimeDeltaMS(start, basis::GetTimestamp());
+
+					start = basis::GetTimestamp();
+					independent(false, tasks, sleep, cpu);
+					auto disabled = basis::GetTimeDeltaMS(start, basis::GetTimestamp());
+
+					printf("%u\t%u\t%u\t%u\t%u\t%.2f\n", tasks, cpu, sleep, enabled, disabled, disabled / (float) enabled);
+					timewaiting[0] = timewaiting[1] = 0;
+				}
 			}
 		}
 	});
@@ -89,23 +157,6 @@ void test_blocking()
 int main(int argc, char * argv[])
 {
 	BASIS_UNUSED(argc, argv);
-
-	uint32_t * data = new uint32_t[MAX_FILE_DIM * MAX_FILE_DIM];
-	for (uint32_t i=0; i<(MAX_FILE_DIM*MAX_FILE_DIM); i++)
-	{
-		data[i] = i;
-	}
-	for (int i=0; i<FILE_COUNT; i++)
-	{
-		char tmp[256];
-		sprintf(tmp, "tmp\\blocking_data%d.dat", i);
-		FILE * dummy = fopen(tmp, "wb");
-		fwrite(data, 4, MAX_FILE_DIM*MAX_FILE_DIM, dummy);
-		fclose(dummy);
-	}
-	delete [] data;
-
 	BASIS_RUN_TESTS();
-
 	return 0;
 }
