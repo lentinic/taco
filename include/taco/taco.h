c816dd28586923c968fc2f24c18a8972929e8076
Chris Lentini <lentinic@gmail.com>
2022-06-23 21:12:30 -0700

    fix include of renamed file

--- include/taco/taco.h
+++ include/taco/taco.h
@@ -7,7 +7,7 @@ This source code is licensed under the MIT license (found in the LICENSE file in
 
 #pragma once
 
-#include "scheduler.h"
+#include "taco_core.h"
 #include "mutex.h"
 #include "shared_mutex.h"
 #include "shared_lock.h"
