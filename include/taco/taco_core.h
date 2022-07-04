74abc9ec2f7af04e457dccd04546f689c8c82dd6
Chris Lentini <lentinic@gmail.com>
2022-07-03 19:35:15 -0700

    Minor OCD

--- include/taco/taco_core.h
+++ include/taco/taco_core.h
@@ -18,26 +18,26 @@ namespace taco
         static constexpr uint32_t invalid_thread_id = 0xffffffff;
     }
 
-    void Initialize(int nthreads = -1);
-    void Initialize(const char * name, task_fn comain, int nthreads = -1);
+    void                Initialize                  (int nthreads = -1);
+    void                Initialize                  (const char * name, task_fn comain, int nthreads = -1);
+    void                Shutdown                    ();
 
-    void Shutdown();
-    void EnterMain();
-    void ExitMain();
+    void                EnterMain                   ();
+    void                ExitMain                    ();
 
-    void Schedule(const char * name, task_fn fn, uint32_t threadid = constants::invalid_thread_id);
+    void                Schedule                    (const char * name, task_fn fn, uint32_t threadid = constants::invalid_thread_id);
 
-    void SetTaskLocalData(void * data);
-    void * GetTaskLocalData();
-    const char * GetTaskName();
-    void Switch();
+    void                SetTaskLocalData            (void * data);
+    void *              GetTaskLocalData            ();
+    const char *        GetTaskName                 ();
+    void                Switch                      ();
     
-    void BeginBlocking();
-    void EndBlocking();
+    void                BeginBlocking               ();
+    void                EndBlocking                 ();
 
-    uint32_t GetThreadCount();
-    uint32_t GetSchedulerId();
-    uint64_t GetTaskId();
+    uint32_t            GetThreadCount              ();
+    uint32_t            GetSchedulerId              ();
+    uint64_t            GetTaskId                   ();
 
     inline void Initialize(task_fn comain, int nthreads = -1)
     {
